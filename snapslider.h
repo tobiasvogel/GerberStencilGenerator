c5a72a3a44f41acc8cb7ffbfd57f9b42d5138623
Tobias X. Vogel <tobias.vogel@linux.com>
2020-05-07 19:31:26 +0200

    added icons

---
+++ snapslider.h
@@ -0,0 +1,41 @@
+#ifndef SNAPSLIDER_H
+#define SNAPSLIDER_H
+
+#include <QSlider>
+#include <QKeyEvent>
+#include <QFocusEvent>
+
+class snapSlider : public QSlider
+{
+    Q_OBJECT
+public:
+    snapSlider(QWidget *parent);
+
+public Q_SLOTS:
+     void setSnapping(int minimum, int maximum, int step);
+
+private Q_SLOTS:
+    void currentValueChanged(int value);
+
+Q_SIGNALS:
+    void snapOn(bool);
+    void snapValueChanged(int);
+
+
+protected:
+    void keyPressEvent(QKeyEvent *ev);
+    void keyReleaseEvent(QKeyEvent *ev);
+    void focusOutEvent(QFocusEvent *event);
+    void setSnapon(void);
+    void setSnapoff(void);
+
+
+private:
+    bool _snapMode = false;
+    signed int _initialMax;
+    signed int _initialMin;
+    int _snapFactor = 10;
+    int _tickInterval = 10;
+};
+
+#endif // SNAPSLIDER_H
